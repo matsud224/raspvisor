188528dfd419a6f750a5aa979a59a2ec3f2e1b9c
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-08 06:50:04 +0900

    enable cache

--- include/arm/mmu.h
+++ include/arm/mmu.h
@@ -1,40 +1,49 @@
 #ifndef _MMU_H
 #define _MMU_H
 
-#define MM_TYPE_PAGE_TABLE 0x3
-#define MM_TYPE_PAGE 0x3
-#define MM_TYPE_BLOCK 0x1
-#define MM_ACCESS (0x1 << 10)
-#define MM_ACCESS_PERMISSION (0x01 << 6)
-#define MM_nG (0x0 << 11)
+#define MM_TYPE_PAGE_TABLE    0x3
+#define MM_TYPE_PAGE          0x3
+#define MM_TYPE_BLOCK         0x1
+
+#define MM_ACCESS             (1 << 10)
+#define MM_nG                 (0 << 11)
+#define MM_SH                 (3 << 8)
+#define MM_ACCESS_PERMISSION  (1 << 6)
 
 /*
  * Memory region attributes:
  *
  *   n = AttrIndx[2:0]
  *			n	MAIR
- *   DEVICE_nGnRnE	000	00000000
- *   NORMAL_NC		001	01000100
+ *   DEVICE_nGnRnE	    000	00000000
+ *   NORMAL_CACHEABLE		001	11111111
  */
-#define MT_DEVICE_nGnRnE 0x0
-#define MT_NORMAL_NC 0x1
-#define MT_DEVICE_nGnRnE_FLAGS 0x00
-#define MT_NORMAL_NC_FLAGS 0x44
+#define MT_DEVICE_nGnRnE           0x0
+#define MT_NORMAL_CACHEABLE        0x1
+
+#define MT_DEVICE_nGnRnE_FLAGS     0x00
+#define MT_NORMAL_CACHEABLE_FLAGS  0xff
+
 #define MAIR_VALUE                                                             \
   (MT_DEVICE_nGnRnE_FLAGS << (8 * MT_DEVICE_nGnRnE)) |                         \
-      (MT_NORMAL_NC_FLAGS << (8 * MT_NORMAL_NC))
+      (MT_NORMAL_CACHEABLE_FLAGS << (8 * MT_NORMAL_CACHEABLE))
 
-#define MMU_FLAGS (MM_TYPE_BLOCK | (MT_NORMAL_NC << 2) | MM_nG | MM_ACCESS)
+
+#define MMU_FLAGS (MM_TYPE_BLOCK | (MT_NORMAL_CACHEABLE << 2) | MM_nG | MM_SH | MM_ACCESS)
 #define MMU_DEVICE_FLAGS                                                       \
-  (MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_nG | MM_ACCESS)
+  (MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_nG | MM_SH | MM_ACCESS)
 
-#define MM_STAGE2_ACCESS (1 << 10)
-#define MM_STAGE2_MEMATTR (1 << 0)
+
+#define MM_STAGE2_ACCESS   (1 << 10)
+#define MM_STAGE2_SH       (3 << 8)
+#define MM_STAGE2_MEMATTR  (1 << 0)
+
 #define MMU_STAGE2_PAGE_FLAGS                                                  \
-  (MM_TYPE_PAGE | MM_STAGE2_ACCESS | MM_STAGE2_MEMATTR)
+  (MM_TYPE_PAGE | MM_STAGE2_ACCESS | MM_STAGE2_SH | MM_STAGE2_MEMATTR)
 
-#define TCR_T0SZ (64 - 48)
-#define TCR_TG0_4K (0 << 14)
-#define TCR_VALUE (TCR_T0SZ | TCR_TG0_4K)
+
+#define TCR_T0SZ    (64 - 48)
+#define TCR_TG0_4K  (0 << 14)
+#define TCR_VALUE   (TCR_T0SZ | TCR_TG0_4K)
 
 #endif
--- include/arm/sysregs.h
+++ include/arm/sysregs.h
@@ -5,11 +5,11 @@
 // SCTLR_EL2, System Control Register (EL2)
 // ***************************************
 
-#define SCTLR_EE (0 << 25)
+#define SCTLR_EE               (0 << 25)
 #define SCTLR_I_CACHE_DISABLED (0 << 12)
 #define SCTLR_D_CACHE_DISABLED (0 << 2)
-#define SCTLR_MMU_DISABLED (0 << 0)
-#define SCTLR_MMU_ENABLED (1 << 0)
+#define SCTLR_MMU_DISABLED     (0 << 0)
+#define SCTLR_MMU_ENABLED      (1 << 0)
 
 #define SCTLR_VALUE_MMU_DISABLED                                               \
   (SCTLR_EE | SCTLR_I_CACHE_DISABLED | SCTLR_D_CACHE_DISABLED |                \
@@ -19,15 +19,16 @@
 // HCR_EL2, Hypervisor Configuration Register (EL2)
 // ***************************************
 
-#define HCR_E2H (0 << 34)
-#define HCR_RW (1 << 31)
-#define HCR_TGE (0 << 27)
-#define HCR_TWI (1 << 13)
-#define HCR_AMO (1 << 5) // routing to EL2
-#define HCR_IMO (1 << 4) // routing to EL2
-#define HCR_FMO (1 << 3) // routing to EL2
-#define HCR_SWIO (1 << 1)
-#define HCR_VM (1 << 0) // stage 2 translation enable
+#define HCR_E2H   (0 << 34)
+#define HCR_RW    (1 << 31)
+#define HCR_TGE   (0 << 27)
+#define HCR_TWI   (1 << 13)
+#define HCR_AMO   (1 << 5) // routing to EL2
+#define HCR_IMO   (1 << 4) // routing to EL2
+#define HCR_FMO   (1 << 3) // routing to EL2
+#define HCR_SWIO  (1 << 1)
+#define HCR_VM    (1 << 0) // stage 2 translation enable
+
 #define HCR_VALUE                                                              \
   (HCR_E2H | HCR_RW | HCR_TGE | HCR_TWI | HCR_AMO | HCR_IMO | HCR_FMO |        \
    HCR_SWIO | HCR_VM)
@@ -37,38 +38,41 @@
 // ***************************************
 
 #define SCR_RESERVED (3 << 4)
-#define SCR_RW (1 << 10)
-#define SCR_NS (1 << 0)
-#define SCR_VALUE (SCR_RESERVED | SCR_RW | SCR_NS)
+#define SCR_RW       (1 << 10)
+#define SCR_NS       (1 << 0)
+
+#define SCR_VALUE    (SCR_RESERVED | SCR_RW | SCR_NS)
 
 // ***************************************
 // SPSR_EL3, Saved Program Status Register (EL3)
 // ***************************************
 
 #define SPSR_MASK_ALL (7 << 6)
-#define SPSR_EL2h (9 << 0)
-#define SPSR_VALUE (SPSR_MASK_ALL | SPSR_EL2h)
+#define SPSR_EL2h     (9 << 0)
+
+#define SPSR_VALUE    (SPSR_MASK_ALL | SPSR_EL2h)
 
 // ***************************************
 // ESR_EL2, Exception Syndrome Register (EL2)
 // ***************************************
 
-#define ESR_EL2_EC_SHIFT 26
-#define ESR_EL2_EC_TRAP_WFx 1
-#define ESR_EL2_EC_HVC64 22
-#define ESR_EL2_EC_DABT_LOW 36
+#define ESR_EL2_EC_SHIFT     26
+
+#define ESR_EL2_EC_TRAP_WFx  1
+#define ESR_EL2_EC_HVC64     22
+#define ESR_EL2_EC_DABT_LOW  36
 
 // ***************************************
 // VTCR_EL2,  Virtualization Translation Control Register (EL2)
 // ***************************************
 
-#define VTCR_NSA (1 << 30)
-#define VTCR_NSW (1 << 29)
-#define VTCR_VS (1 << 19)
-#define VTCR_PS (5 << 16)
-#define VTCR_TG0 (0 << 14) // 4KB
-#define VTCR_SL0 (2 << 6)
-#define VTCR_T0SZ (64 - 48)
+#define VTCR_NSA   (1 << 30)
+#define VTCR_NSW   (1 << 29)
+#define VTCR_VS    (1 << 19)
+#define VTCR_PS    (5 << 16)
+#define VTCR_TG0   (0 << 14) // 4KB
+#define VTCR_SL0   (2 << 6)
+#define VTCR_T0SZ  (64 - 48)
 #define VTCR_VALUE                                                             \
   (VTCR_NSA | VTCR_NSW | VTCR_VS | VTCR_PS | VTCR_TG0 | VTCR_SL0 | VTCR_T0SZ)
 

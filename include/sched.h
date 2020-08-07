4bc541e7ea0d433a6253a13dfff16f7d788af8a9
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-07 18:29:13 +0900

    add comment

--- include/sched.h
+++ include/sched.h
@@ -68,7 +68,7 @@ struct task_struct {
 	long counter;
 	long priority;
 	long preempt_count;
-  long id;
+  long id;  // used as VMID
 	unsigned long flags;
 	struct mm_struct mm;
   struct cpu_sysregs cpu_sysregs;

9d1597f2ed19bad86831e2ef62c696afb20baca1
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-10 00:09:11 +0900

    remove unused constants

--- include/entry.h
+++ include/entry.h
@@ -3,22 +3,20 @@
 #define S_FRAME_SIZE 272 // size of all saved registers
 #define S_X0 0           // offset of x0 register in saved stack frame
 
-#define SYNC_INVALID_EL2 0
-#define IRQ_INVALID_EL2 1
-#define FIQ_INVALID_EL2 2
-#define ERROR_INVALID_EL2 3
+#define SYNC_INVALID_EL2      0
+#define IRQ_INVALID_EL2       1
+#define FIQ_INVALID_EL2       2
+#define ERROR_INVALID_EL2     3
 
-#define FIQ_INVALID_EL01_64 6
+#define SYNC_INVALID_EL01_64  4
+#define IRQ_INVALID_EL01_64   5
+#define FIQ_INVALID_EL01_64   6
 #define ERROR_INVALID_EL01_64 7
 
-#define SYNC_INVALID_EL01_32 8
-#define IRQ_INVALID_EL01_32 9
-#define FIQ_INVALID_EL01_32 10
+#define SYNC_INVALID_EL01_32  8
+#define IRQ_INVALID_EL01_32   9
+#define FIQ_INVALID_EL01_32   10
 #define ERROR_INVALID_EL01_32 11
-
-#define SYNC_ERROR 12
-#define HVC_ERROR 13
-#define DATA_ABORT_ERROR 14
 
 #ifndef __ASSEMBLER__
 

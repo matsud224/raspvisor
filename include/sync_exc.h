1b95b9b0cc702c1d522afa43455ed5103b2ee638
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-09 16:55:01 +0900

    split irq.c

---
+++ include/sync_exc.h
@@ -0,0 +1,7 @@
+#ifndef _SYNC_EXC_H
+#define _SYNC_EXC_H
+
+void show_uncaught_sync_exception_message(int type, unsigned long esr,
+                                unsigned long address);
+
+#endif /*_SYNC_EXC_H */

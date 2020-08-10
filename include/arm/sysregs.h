e1f11325ef8a554b777a1974ec8c54f6a17106a8
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-10 15:38:57 +0900

    fix HCR_EL2

--- include/arm/sysregs.h
+++ include/arm/sysregs.h
@@ -18,22 +18,38 @@
 // HCR_EL2, Hypervisor Configuration Register (EL2)
 // ***************************************
 
-#define HCR_E2H   (0 << 34)
-#define HCR_RW    (1 << 31)
-#define HCR_TRVM  (1 << 30)
-#define HCR_TGE   (0 << 27)
-#define HCR_TWI   (1 << 13)
-#define HCR_AMO   (1 << 5) // routing to EL2
-#define HCR_IMO   (1 << 4) // routing to EL2
-#define HCR_FMO   (1 << 3) // routing to EL2
-#define HCR_SWIO  (1 << 1)
-#define HCR_VM    (1 << 0) // stage 2 translation enable
+// trap related
+#define HCR_TID5    (1 << 58)
+#define HCR_ENSCXT  (0 << 53)
+#define HCR_TID4    (1 << 49)
+#define HCR_FIEN    (0 << 47)
+#define HCR_TERR    (1 << 36)
+#define HCR_TLOR    (1 << 35)
+#define HCR_TRVM    (0 << 30)
+#define HCR_TDZ     (1 << 28)
+#define HCR_TVM     (1 << 26)
+#define HCR_TACR    (1 << 21)
+#define HCR_TID3    (1 << 18)
+#define HCR_TID2    (1 << 17)
+#define HCR_TID1    (1 << 16)
+#define HCR_TWE     (1 << 14)
+#define HCR_TWI     (1 << 13)
+// others
+#define HCR_E2H     (0 << 34)
+#define HCR_RW      (1 << 31)
+#define HCR_TGE     (0 << 27)
+#define HCR_AMO     (1 << 5) // routing to EL2
+#define HCR_IMO     (1 << 4) // routing to EL2
+#define HCR_FMO     (1 << 3) // routing to EL2
+#define HCR_SWIO    (1 << 1)
+#define HCR_VM      (1 << 0) // stage 2 translation enable
 
-#define HCR_VALUE                                                              \
-  (HCR_E2H | HCR_RW | HCR_TGE | HCR_TWI | HCR_AMO | HCR_IMO | HCR_FMO |        \
-   HCR_SWIO | HCR_VM)
+#define HCR_VALUE  \
+  (HCR_TID5 | HCR_ENSCXT | HCR_TID4 | HCR_FIEN | HCR_TERR | HCR_TLOR |  \
+   HCR_TRVM | HCR_TDZ | HCR_TVM | HCR_TACR | HCR_TID3 | HCR_TID2 | HCR_TID1 |  \
+   HCR_TWE | HCR_TWI | HCR_E2H | HCR_RW | HCR_TGE | HCR_AMO | HCR_IMO |  \
+   HCR_FMO | HCR_SWIO | HCR_VM)
 
-// ***************************************
 // SCR_EL3, Secure Configuration Register (EL3)
 // ***************************************
 

802877fdd8e924768ab98ba44c710af4ddb80cfa
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-08-14 23:51:55 +0900

    arrange format of sd.c, use put/get32

--- include/sd.h
+++ include/sd.h
@@ -23,9 +23,9 @@
  *
  */
 
-#define SD_OK                0
-#define SD_TIMEOUT          -1
-#define SD_ERROR            -2
+#define SD_OK 0
+#define SD_TIMEOUT -1
+#define SD_ERROR -2
 
 int sd_init();
 int sd_readblock(unsigned int lba, unsigned char *buffer, unsigned int num);
